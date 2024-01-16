8f75bc3bebc729e5697fbd844d91bb0d66ace049
jiseong0612 <hth5545@gmail.com>
2024-01-16 18:11:13 +0900

    비동기 요청하기

---
+++ vuestagram/src/components/Container.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <Post :posts="post" v-for="(post, i ) in posts" :key="i"></Post>
+  </div>
+
+</template>
+
+<script>
+import Post from './Post.vue';
+
+export default {
+  name:"Containner",
+  data(){
+
+  },
+  props:{
+    posts: Array,
+  },
+  components:{
+    Post
+  }
+}
+</script>
+
+<style>
+
+</style>
